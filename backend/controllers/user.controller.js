7867176e4cd8b9906648764cc8847ddc4053d923
Aman kumar singh <157352533+aman-singh0409@users.noreply.github.com>
2025-10-30 22:11:05 +0530

    30.10.2025

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import {User} from "../model/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from"jsonwebtoken";
 
 export const register=async (req,res)=>{
     try {
@@ -13,7 +14,7 @@ export const register=async (req,res)=>{
         const user =await User.findOne({email});
         if(user){
             return res.status(400).json({
-                message:'User already exist with this email.',
+                message:"User already exist with this email.",
                 success:false
             })
         } 
@@ -24,9 +25,68 @@ export const register=async (req,res)=>{
             phoneNumber,
             password:hashedPassword,
             role
-        })
+        });
+        return res.status(201).json({
+            message:"Account created successfully.",
+            success:true
+        });
+
 
     } catch (error) {
+        console.log(error);
         
+    }
+}
+export const login=async (req, res) =>{
+    try {
+        const {email,password,role}=req.body;
+        if(!email || !password || !role){
+            return res.status(400).json({
+                message:"Missing values",
+                success:false
+            });
+        };
+        let user =await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message:"Incorrect email or password",
+                success:false
+            })
+        } 
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                message:"Incorrect email or password",
+                success:false
+            })
+        };
+        if(role!=user.role){
+            return res.status(400).json({
+                message:"Account does not exist with current role",
+                success:false
+            })
+        };
+
+        const tokenData={
+            userId:user._id
+        }
+        const token=await jwt.sign(tokenData, process.env.SECRET_KEY,{expiresIn:'1d'});
+
+        user={
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile:user.profile
+        }
+
+        return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000, httpsOnly:true, sameSite:'strict'}).json({
+            message:`Welcome back ${user.fullname}`,
+            user,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
